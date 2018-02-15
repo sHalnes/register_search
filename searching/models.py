e9b3f13703c5b59f4be8a3cea6e6d6d15dc4a096
Svetlana Halnes <s_halnes@Svetlanas-MacBook-Air.local>
2018-02-15 21:59:11 +0100

    commit 6

--- searching/models.py
+++ searching/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class SearchResults(models.Model):
+    '''
+    A database of companies which were searched via the searching app. Just in case.
+    '''
+
+    id = models.IntegerField(verbose_name='Organizarion number', primary_key=True)
+    org_name = models.CharField(max_length=100)
+    last_visited = models.DateTimeField(auto_now=True)
+    n_visits = models.IntegerField(default=0)
+
+
+    def __str__(self):
+        '''
+        representing model
+        :return: self.org_name, self.n_visits, self.last_visited
+        '''
+        return '{} vas visited {} times. Last visit: {}'.format(self.org_name, self.n_visits, self.last_visited)
