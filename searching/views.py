538a8b0ec2678c6b1424a56fc2af450bfa38d540
Svetlana Halnes <s_halnes@Svetlanas-MacBook-Air.local>
2018-02-17 20:17:18 +0100

    commit 6

--- searching/views.py
+++ searching/views.py
@@ -7,15 +7,15 @@ def index(request):
     if request.method == 'POST':
         search_text = request.POST['search_text']
     else:
-        search_text = ''
-#    org_num = 'Kart'
+        #search_text = ''
+        search_text = 'Kart'
 #    org_num = '912660680'
 
     data = find_organization(search_text)
     if not data:
         data = ['Foretak med organisasjosnummeret {} er ikke registrert'.format(search_text)]
-    #return render(request, 'search.html', context={'data':data})
-    return render(request, 'index.html',context={'data': data})
+    return render(request, 'search.html', context={'data':data})
+    #return render(request, 'index.html',context={'data': data})
 
 def search(request):
     pass
