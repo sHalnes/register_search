9c2f02996cd7f3d898b88e18844d83e13e2edb7c
Svetlana Halnes <s_halnes@Svetlanas-MacBook-Air.local>
2018-02-17 15:42:58 +0100

    commit 6

--- searching/brreg_API.py
+++ searching/brreg_API.py
@@ -22,8 +22,10 @@ def find_organization(input_string):
         if response.status_code == 200:
             try:
                 raw_data = json.loads(response.text)
+                print(raw_data)
                 data_to_return = get_data_from_jsson(raw_data)
             except(ValueError, KeyError, TypeError):
+                print('something went wrong')
                 return 0
         else:
             url = 'http://data.brreg.no/enhetsregisteret/underenhet'
@@ -32,9 +34,12 @@ def find_organization(input_string):
             if response.status_code == 200:
                 try:
                     raw_data = json.loads(response.text)
+                    print(raw_data)
                     data_to_return = get_data_from_jsson(raw_data)
                 except(ValueError, KeyError, TypeError):
+                    print('something went wrong')
                     return 0
+    #print(data_to_return)
     return data_to_return
 
 
@@ -75,17 +80,18 @@ def get_data_from_jsson(raw_data):
 
     # retrieve data from raw json
     for key in json_keys:
-        if key in raw_data:
-            data_str = keys_description[key]
-            #print(key)
+        #print(key)
+        if key in raw_data and key in keys_description:
+            data_str = ''
+            data_str += keys_description[key]
             if len(json_keys[key]) > 1:
                 for underkey in range(1, len(json_keys[key])):
                     if json_keys[key][underkey] in raw_data[key]:
-                        data_str +=('\n'+raw_data[key][json_keys[key][underkey]])
+                        data_str += str(raw_data[key][json_keys[key][underkey]])
                         #data_to_return.append(raw_data[key][json_keys[key][underkey]])
                         #print(raw_data[key][json_keys[key][underkey]])
             else:
-                data_str += ('\n' + raw_data[key])
+                data_str += str(raw_data[key])
                 #data_to_return.append(raw_data[key])
                 #print(raw_data[key])
             data_to_return.append(data_str)
@@ -94,8 +100,8 @@ def get_data_from_jsson(raw_data):
 
 
 #print('underenhet')
-#find_organization('874714852')
+find_organization('874714852')
 #print('\nenhet')
-#find_organization('974760673')
+find_organization('974760673')
 #print('\nenhet')
 #find_organization('912660680')
