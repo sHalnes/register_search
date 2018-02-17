63a060c06047c920e490caa5ae3db085459cf5db
Svetlana Halnes <s_halnes@Svetlanas-MacBook-Air.local>
2018-02-17 23:21:45 +0100

    commit 6

--- searching/urls.py
+++ searching/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from searching.views import SearchSubmitView
 from . import views
 
 urlpatterns = [
     #path('', views.index, name='index'),
-    path('', views.SearchSubmitView, name='index'),
+    path('', SearchSubmitView.as_view()),
     #path('search/', views.search, name='search'),
     ]
