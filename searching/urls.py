43b38dfa7fb870b4521659d5e64a683c42e7f007
Svetlana Halnes <s_halnes@Svetlanas-MacBook-Air.local>
2018-02-18 13:06:11 +0100

    commit 6

--- searching/urls.py
+++ searching/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     #path('', SearchSubmitView.as_view()),
-    #path('search/', views.search, name='search'),
+    path('search/', views.search, name='search'),
     ]
