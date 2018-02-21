b81d625ce6021ded830883f5662dbde694766c7d
Svetlana Halnes <s_halnes@Svetlanas-MacBook-Air.local>
2018-02-21 21:10:29 +0100

    commit 6

--- searching/views.py
+++ searching/views.py
@@ -34,26 +34,27 @@ def index(request):
     #return render(request, 'index.html', context={'title':response_message, 'data': data, 'reg_num':reg_num})
 
 def search(request):
-    response_message = ''
-    query = request.POST.get('search', '')
-    data = []
-    reg_num = False
-    # we begin search as soon as number of letters is bigger than 2
-    if len(query) > 2:
-        reg_num, data = find_organization(query)
-#        reg_num, data, adresse = find_organization(query)
-    # in case we cannot find data about reg number or organization's name
-    if (len(query) == 9 and query.isnumeric() and data == 0) or (len(query) > 2 and not query.isnumeric() and data == 0):
-        response_message = "Ingen treff"
-    elif len(query) > 9 and query.isnumeric():
-        response_message = "Feil reg. nummer"
-    # print out number of results
-    elif len(query) > 2 and not query.isnumeric():
-        response_message = "Det finnes " + str(len(data)) + " treff: "
-    context = {'message':response_message, 'query':query, 'data':data, 'reg_num': reg_num}
-    rendered_template = render(request, 'search.html',context)
+#     response_message = ''
+#     query = request.POST.get('search', '')
+#     data = []
+#     reg_num = False
+#     # we begin search as soon as number of letters is bigger than 2
+#     if len(query) > 2:
+#         reg_num, data = find_organization(query)
+# #        reg_num, data, adresse = find_organization(query)
+#     # in case we cannot find data about reg number or organization's name
+#     if (len(query) == 9 and query.isnumeric() and data == 0) or (len(query) > 2 and not query.isnumeric() and data == 0):
+#         response_message = "Ingen treff"
+#     elif len(query) > 9 and query.isnumeric():
+#         response_message = "Feil reg. nummer"
+#     # print out number of results
+#     elif len(query) > 2 and not query.isnumeric():
+#         response_message = "Det finnes " + str(len(data)) + " treff: "
+#     context = {'message':response_message, 'query':query, 'data':data, 'reg_num': reg_num}
+#     rendered_template = render(request, 'search.html',context)
+#     return HttpResponse(rendered_template, content_type='text/html')
+    rendered_template = render(request, 'search.html',context = {})
     return HttpResponse(rendered_template, content_type='text/html')
-
 
 def orgview(request):
     '''
