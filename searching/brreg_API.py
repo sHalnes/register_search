43939736bd5b58a56d4e7e8c597655fa858157d4
Svetlana Halnes <s_halnes@Svetlanas-MacBook-Air.local>
2018-02-24 20:05:16 +0100

    commit 6

--- searching/brreg_API.py
+++ searching/brreg_API.py
@@ -1,48 +1,7 @@
 # -*- coding: utf-8 -*-
-#import json
-#import requests
-#from . import kartverket_API
-
-
 import json
 import requests
-
-def get_geodata(address):
-    '''
-    Find and return lattitude and longitude to the first matching address from geonorge API
-    :param address: (street, building, postnumber, poststed, kommunenumber, kommune)
-    :return: float lat, lon, or (-1,-1) if the coords cannot be found
-    '''
-    street, building, postnumber, poststed, kommunenumber, kommune = address
-    lat = lon = '0.0'
-    url = 'http://ws.geonorge.no/AdresseWS/adresse/sok?sokestreng={}%20{},{}'.format(street,building,kommune.lower())
-    response = requests.get(url)
-    if response.status_code == 200:
-        try:
-            raw_data = json.loads(response.text)
-            lat, lon = find_coords(raw_data,address)
-        except(ValueError, KeyError, TypeError):
-            return (-1,-1)
-    return (float(lat), float(lon))
-
-def find_coords(raw_data, address):
-    street, building, postnumber, poststed, kommunenumber, kommune = address
-    for i in range(len(raw_data['adresser'])):
-        if (raw_data['adresser'][i]['adressenavn'] == street and
-                raw_data['adresser'][i]['husnr'] == building and
-                raw_data['adresser'][i]['postnr'] == postnumber and
-                raw_data['adresser'][i]['poststed'] == poststed and
-                raw_data['adresser'][i]['kommunenr'] == kommunenumber and
-                raw_data['adresser'][i]['kommunenavn'] == kommune):
-            lat = raw_data['adresser'][i]['nord']
-            lon = raw_data['adresser'][i]['aust']
-            return (lat, lon)
-    return (-1,-1)
-
-
-
-
-
+from .kartverket_API import get_geodata
 
 
 def find_organization(input_string):
@@ -52,7 +11,6 @@ def find_organization(input_string):
     :return: data if exist, zero if there is no data + a boolean variable to describe kind of data
     '''
     data_to_return = []
-    #address = ()
     lat_lon = (-1,-1)
     reg_num = False
     url_enhet = 'http://data.brreg.no/enhetsregisteret/enhet'
--- searching/kartverket_API.py
+++ searching/kartverket_API.py
@@ -5,11 +5,10 @@ def get_geodata(address):
     '''
     Find and return lattitude and longitude to the first matching address from geonorge API
     :param address: (street, building, postnumber, poststed, kommunenumber, kommune)
-    :return: float lat, lon
+    :return: float lat, lon, or (-1,-1) if the coords cannot be found
     '''
     street, building, postnumber, poststed, kommunenumber, kommune = address
     lat = lon = '0.0'
-
     url = 'http://ws.geonorge.no/AdresseWS/adresse/sok?sokestreng={}%20{},{}'.format(street,building,kommune.lower())
     response = requests.get(url)
     if response.status_code == 200:
@@ -17,8 +16,8 @@ def get_geodata(address):
             raw_data = json.loads(response.text)
             lat, lon = find_coords(raw_data,address)
         except(ValueError, KeyError, TypeError):
-            return 0
-    return (lat, lon)
+            return (-1,-1)
+    return (float(lat), float(lon))
 
 def find_coords(raw_data, address):
     street, building, postnumber, poststed, kommunenumber, kommune = address
@@ -32,44 +31,4 @@ def find_coords(raw_data, address):
             lat = raw_data['adresser'][i]['nord']
             lon = raw_data['adresser'][i]['aust']
             return (lat, lon)
-    return 0
-
-
-
-#get_geodata('Haldenveien', '67', 'ringerike')
-#address = ('Havnegata','48','8900', 'BRØNNØYSUND','1813', 'BRØNNØY')
-#a,b = get_geodata(address)
-
-'''
-
-{'sokStatus': {'ok': 'true', 'melding': ''},
-'totaltAntallTreff': '37453', 'adresser':
-[{'type': 'Vegadresse',
-'adressekode': '8000',
-'adressenavn': 'Haldenveien',
-'kortadressenavn': 'Haldenveien',
-'husnr': '67',
-'undernr': '0',
-'postnr': '3515',
-'poststed': 'HØNEFOSS',
-'kommunenr': '0605',
- 'kommunenavn': 'RINGERIKE',
- 'gardsnr': '87',
- 'bruksnr': '321',
- 'festenr': '0',
- 'seksjonsnr': '0',
- 'bruksnavn': 'FLATA',
- 'nord': '60.18321133466702',
- 'aust': '10.254246883657563'
-
-
-
-'adresse': 'Havnegata 48',
-    'postnummer': '8900',
-    'poststed': 'BRØNNØYSUND',
-    'kommunenummer': '1813',
-    'kommune': 'BRØNNØY',
-
-http://ws.geonorge.no/AdresseWS/adresse/sok?sokestreng=Haldenveien%2067,ringerike
-
-'''
+    return (-1,-1)
