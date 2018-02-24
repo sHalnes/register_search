9b733e8bbf0395dfb9a546d4e1a723caffc6bbd8
Svetlana Halnes <s_halnes@Svetlanas-MacBook-Air.local>
2018-02-24 20:01:30 +0100

    commit 6

--- searching/views_old.py
+++ searching/views_old.py
@@ -6,6 +6,23 @@ from django.http import HttpResponse
 # this was a try to implement map
 
 def index(request):
+    #response_message = 'This is the response from index'
+    query = request.POST.get('search', '')
+
+    #if len(query) == 9 and query.isnumeric():
+    #    return render(request, 'index.html', context={'reg': True})
+
+    #data = []
+    #reg_num = False
+    # a simple query. not sure I need it here
+    #reg_num, data = find_organization(query)
+
+#    reg_num, data, adresse = find_organization(query)
+    #return render(request, 'index.html', context={'title':response_message, 'data': data, 'reg_num':reg_num})
+    return render(request, 'index.html', context={})
+
+
+def search(request):
     response_message = ''
     query = request.POST.get('search', '')
     data = []
@@ -17,24 +34,16 @@ def index(request):
         reg_num, data, latlon = find_organization(query)
         lat = latlon[0]
         lon = latlon[1]
-    #        reg_num, data, adresse = find_organization(query)
+#        reg_num, data, adresse = find_organization(query)
     # in case we cannot find data about reg number or organization's name
-    if (len(query) == 9 and query.isnumeric() and data == 0) or (
-            len(query) > 2 and not query.isnumeric() and data == 0):
+    if (len(query) == 9 and query.isnumeric() and data == 0) or (len(query) > 2 and not query.isnumeric() and data == 0):
         response_message = "Ingen treff"
     elif len(query) > 9 and query.isnumeric():
         response_message = "Feil reg. nummer"
     # print out number of results
     elif len(query) > 2 and not query.isnumeric():
         response_message = "Det finnes " + str(len(data)) + " treff: "
-    context = {'message': response_message, 'query': query, 'data': data, 'reg_num': reg_num, 'lat': lat, 'lon': lon}
-    rendered_template = render(request, 'index.html', context)
-    return HttpResponse(rendered_template, content_type='text/html')
-
-
-def search(request):
-
-    context = {}
+    context = {'message':response_message, 'query':query, 'data':data, 'reg_num': reg_num,'lat':lat, 'lon':lon}
     rendered_template = render(request, 'search.html',context)
     return HttpResponse(rendered_template, content_type='text/html')
 
